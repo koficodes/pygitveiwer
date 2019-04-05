83f98fa6fff3b45440a290f06d482c33217c499f
Paul Karikari <paulkarikari1@gmail.com>
2019-04-05 07:41:14 +0200

    fix trailing / inconsistency

--- pygitviewer.py
+++ pygitviewer.py
@@ -4,6 +4,7 @@ import click
 
 
 def list_dir_repositories(path):
+    path = os.path.join(path, '')
     repositories = [discover_repository(path+dir_name)
                     for dir_name in os.listdir(path)]
 
@@ -11,7 +12,7 @@ def list_dir_repositories(path):
 
 
 def get_current_branch_name(repo_path):
-    repo = Repository(repo_path.rstrip('/'))
+    repo = Repository(repo_path)
     try:
         return repo.head.shorthand
     except GitError:
@@ -21,8 +22,9 @@ def get_current_branch_name(repo_path):
 @click.command()
 @click.argument('path', default='.', metavar='The directory path of git repositories')
 def display_git_branches(path):
-    print({get_current_branch_name(repo_path): repo_path
-           for repo_path in list_dir_repositories(path)})
+    if os.path.exists(path) and os.path.isdir(path):
+        print({get_current_branch_name(repo_path): repo_path
+               for repo_path in list_dir_repositories(path)})
 
 
 if __name__ == "__main__":
